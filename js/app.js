cf8aafb4cb1298d5fb4b150f54e048d8ae20dc30
ianchan3 <ianchan3138@gmail.com>
2023-01-04 13:13:41 -0800

    Day 3 of 100 Days of JavaScript Completed - Logic Cleanup

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
 const hex = document.querySelector(".hex");
 const btn = document.querySelector(".generate");
 
-const generateColor = () => {
+btn.addEventListener("click", () => {
   const randomColor = Math.random().toString(16).substring(2, 8);
   document.body.style.backgroundColor = `#${randomColor}`;
   hex.innerHTML = `#${randomColor}`;
-}
-
-btn.addEventListener("click", generateColor);
-
-generateColor();
+});
